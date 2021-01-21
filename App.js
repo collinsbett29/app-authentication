3c068bad9918f2009c975662a6774fd35da1e7ad
collinsbett29 <collinsbegtt29@gmail.com>
2021-01-21 11:45:41 +0300

    Added routes to pages

--- App.js
+++ App.js
@@ -1,21 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  View,
+  StatusBar
+} from 'react-native';
+ 
+import Routes from './src/Routes';
+ 
+export default class App extends Component   {
+  render() {
+    return (
+      <View style={styles.container}>
+        <StatusBar
+          backgroundColor="#002f6c" 
+          barStyle="light-content"
+        />
+        <Routes/>
+      </View>
+    );
+  }
 }
-
+ 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  }
 });
